01438e5c6ee8e5ebc6a2449ab5556cff1d0496ac
Luis Magalhaes <sprintlmaga@gmail.com>
2019-10-29 16:00:17 +0000

    Created the CircleCalc method

--- BasicsCSharp/Program.cs
+++ BasicsCSharp/Program.cs
@@ -8,7 +8,7 @@ namespace BasicsCSharp
         {
             ObjString();
             ObjMath();
-
+            CircleCalc();
         }
 
         static void ObjString()
@@ -44,6 +44,7 @@ namespace BasicsCSharp
 
         static void ObjMath()
         {
+            // Integer types variables
             int a = 7;
             int b = 4;
             int c = 3;
@@ -58,6 +59,45 @@ namespace BasicsCSharp
 
             int what = max + 3;
             Console.WriteLine($"Example of overflow: {what}");
+
+            // Double types variables
+            double a1 = 19;
+            double b1 = 23;
+            double c1 = 8;
+            double d1 = (a1 + b1) / c1;
+            Console.WriteLine(d1);
+
+            double dmax = double.MaxValue;
+            double dmin = double.MinValue;
+            Console.WriteLine($"The range of double is {dmin} to {dmax}");
+
+            // Decimal types variables
+            decimal ddmin = decimal.MinValue;
+            decimal ddmax = decimal.MaxValue;
+            Console.WriteLine($"The range of the decimal type is {ddmin} to {ddmax}");
+
+            double a2 = 1.0;
+            double b2 = 3.0;
+            Console.WriteLine(a2 / b2);
+
+            // The M suffix on the numbers is how you indicate that a constant should use the decimal type
+            decimal c2 = 1.0M;
+            decimal d2 = 3.0M;
+            Console.WriteLine(c2 / d2);
         }
+
+        static void CircleCalc()
+        {
+            double radius = 2.50;
+            double wPI = Math.PI;
+            //double SqrRadius = Math.Sqrt(radius);
+            double SqrRadius = radius * radius;
+            double area = SqrRadius * wPI;
+
+            Console.WriteLine($"The area for a circle with a radius of {radius} is {area}");
+
+
+        }
+
     }
 }
